d29e22a0c312685fcc14769769d57733fca4b42a
MC-AGANG <54347161+MC-AGANG@users.noreply.github.com>
2021-08-05 13:05:21 +0800

    Add files via upload

---
+++ 自律神器/Form1.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(31, 47)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(118, 59)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "隐藏"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(187, 155)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+End Class
---
+++ 自律神器/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick                    '利用Timer控件每隔一段时间运行一次，若要修改间隔，请修改Timer1的Interval属性
+
+        Dim flag As Boolean
+        flag = False
+        Dim processlist() As System.Diagnostics.Process = System.Diagnostics.Process.GetProcesses()
+        For Each proc In processlist
+            If proc.ProcessName = "Minecraft Note Block Studio" Or proc.ProcessName = "Code" Then    '检测到两个引号内的进程则不会蓝屏，Minecraft Note Block Studio代表NBS，Code代表Visual Studio Code
+                Exit Sub                                                                             '退出当前程序，跳过后面的步骤
+            End If
+        Next
+        For Each proc In processlist
+            If proc.ProcessName = "javaw" Or proc.ProcessName = "java" Then                          '检测到引号内的进程则触发后续程序，javaw代表HMCL等需要Java的启动器或其他软件，java代表MC等需要Java运行的软件
+                flag = True
+            End If
+        Next
+        If flag = True Then
+            For Each proc In processlist
+                If proc.ProcessName = "svchost" Then                                                 '引号内为需要结束的进程，填入svchost则会导致电脑蓝屏
+                    proc.Kill()
+                End If
+            Next
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click                '点击隐藏按钮后窗体隐藏并显示系统托盘图标
+        Me.Visible = False
+        NotifyIcon1.Visible = True
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        NotifyIcon1.Icon = Me.Icon                                                                   '为任务栏图标赋值，若删除此语句会导致托盘图标无法显示
+    End Sub
+
+    Private Sub NotifyIcon1_Click(sender As Object, e As EventArgs) Handles NotifyIcon1.Click        '点击托盘内的图标隐藏图标并显示窗口
+        Me.Visible = True
+        NotifyIcon1.Visible = False
+    End Sub
+End Class
